b2f2a03a85b689ae7e352c673f551cc5fd2604bb
Wayfarer706 <ugajnazar@gmail.com>
2026-07-02 18:38:52 +0200

    feat: add initial templates and stylesheets for the main page

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from snippet import posts
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"message": "Welcome to the FastAPI application!"}
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/", response_class=HTMLResponse, include_in_schema=False)
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="home.html", context={"posts": posts, "title": "Home"}
+    )
---
+++ blog/snippet.py
@@ -0,0 +1,20 @@
+posts: list[dict[str, str | int]] = [
+    {
+        "id": 1,
+        "title": "First Post",
+        "content": "This is the content of the first post.",
+        "date_posted": "2024-06-01",
+    },
+    {
+        "id": 2,
+        "title": "Second Post",
+        "content": "This is the content of the second post.",
+        "date_posted": "2024-06-02",
+    },
+    {
+        "id": 3,
+        "title": "Third Post",
+        "content": "This is the content of the third post.",
+        "date_posted": "2024-06-03",
+    },
+]
